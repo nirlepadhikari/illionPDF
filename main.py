4d8e28fb9005e6adbf71dfb1e8b5521434efb145
nirlepadhikari <71162816+nirlepadhikari@users.noreply.github.com>
2022-08-30 16:08:18 +1000

    second

--- main.py
+++ main.py
@@ -9,6 +9,21 @@ if file is not None:
     page = reader.pages[0]
     a = (page.extract_text()).split("\n")
     score = a[a.index('A score of ')+1]
+
+    Adverse = a[a.index('Adverse')+1]
+    Defaults = a[a.index('Defaults (Total amount)')+1]
+    if "-" in Defaults:
+        Defaults = 0
+    Default_Amount = a[a.index('Defaults (Total amount)')+3] 
+    if ("""(-)""") in Default_Amount:
+        Default_Amount = 0
+    Enqs = a[a.index('Enquiries (Total amount)')+1]
+
+
     st.snow()
     st.metric('Score', score)
+    st.metric('Adverse', Adverse)
+    st.metric('Defaults', Defaults)
+    st.metric('Default Amount', Default_Amount)
+    st.metric('Enquiries', Enqs)
     
